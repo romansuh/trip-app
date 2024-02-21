df47a3fc4549f6b8b0f2a62e752f61e7f7bccde3
romansuh <72558747+romansuh@users.noreply.github.com>
2024-02-21 19:36:40 +0200

    fix selected trip weather info styles

---
+++ src/common/icons/weatherIconsColor/weatherIcons.js
@@ -0,0 +1,47 @@
+import clearDay from "./clear-day.svg";
+import clearNight from "./clear-night.svg";
+import cloudy from "./cloudy.svg";
+import fog from "./fog.svg";
+import hail from "./hail.svg";
+import partlyCloudyDay from "./partly-cloudy-day.svg";
+import partlyCloudyNight from "./partly-cloudy-night.svg";
+import rain from "./rain.svg";
+import rainSnow from "./rain-snow.svg";
+import rainSnowShowersDay from "./rain-snow-showers-day.svg";
+import rainSnowShowersNight from "./rain-snow-showers-night.svg"
+import showersDay from "./showers-day.svg";
+import showersNight from "./showers-night.svg"
+import sleet from "./sleet.svg";
+import snow from "./snow.svg";
+import snowShowersDay from "./snow-showers-day.svg";
+import snowShowersNight from "./snow-showers-night.svg"
+import thunder from "./thunder.svg";
+import thunderRain from "./thunder-rain.svg";
+import thunderShowersDay from "./thunder-showers-day.svg";
+import thunderShowersNight from "./thunder-showers-night.svg"
+import wind from "./wind.svg";
+
+export default {
+    "clear-day": clearDay,
+    "clear-night": clearNight,
+    "cloudy": cloudy,
+    "fog": fog,
+    "hail": hail,
+    "partly-cloudy-day": partlyCloudyDay,
+    "partly-cloudy-night": partlyCloudyNight,
+    "rain": rain,
+    "rain-snow": rainSnow,
+    "rain-snow-showers-day": rainSnowShowersDay,
+    "rain-snow-showers-night": rainSnowShowersNight,
+    "showers-day": showersDay,
+    "showers-night": showersNight,
+    "sleet": sleet,
+    "snow": snow,
+    "snow-showers-day": snowShowersDay,
+    "snow-showers-night": snowShowersNight,
+    "thunder": thunder,
+    "thunder-rain": thunderRain,
+    "thunder-showers-day": thunderShowersDay,
+    "thunder-showers-night": thunderShowersNight,
+    "wind": wind,
+}
